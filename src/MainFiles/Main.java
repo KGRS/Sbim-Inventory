945d546b7c7118cff3c3325c36d3779e71f27ae9
KGRS <KGRS@192.168.1.6>
2016-04-20 19:07:39 +0530

    inv first commit 2016-4-20

--- src/MainFiles/Main.java
+++ src/MainFiles/Main.java
@@ -35,25 +35,5 @@ public class Main {
         path = System.getProperty("user.dir");
         LoginPage login = new LoginPage();
         login.setVisible(true);
-
     }
-
-//    public static boolean isCheckModule(String userName) {
-//        boolean value = false;
-//        try {
-//            Statement stmt = ConnectSql.conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
-//            ResultSet rset1 = stmt.executeQuery("SELECT * FROM tblUserRoles where UserID = '" + userName + "' AND ModuleCode = '7'");  // 
-//            if (rset1.next()) {
-//                String getCode = rset1.getString("ModuleCode");
-//                if (getCode.equals("7")) {
-//                    value = true;
-//                }
-////                }
-//            }
-//        } catch (SQLException ex) {
-//            JOptionPane.showMessageDialog(null, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-//            JOptionPane.showMessageDialog(null, "please contact for support.");
-//        }
-//        return value;
-//    }
 }
